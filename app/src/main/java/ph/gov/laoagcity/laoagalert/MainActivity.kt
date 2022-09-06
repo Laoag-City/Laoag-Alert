3bd0ea64344b530d3315e94c24e49b5fe9eeccab
Richard L. Alhama <richard.alhama@gmail.com>
2022-09-06 15:12:17 +0800

    Initial working commit

---
+++ app/src/main/java/ph/gov/laoagcity/laoagalert/MainActivity.kt
@@ -0,0 +1,213 @@
+package ph.gov.laoagcity.laoagalert
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ph.gov.laoagcity.laoagalert.ui.theme.LaoagAlertTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LaoagAlertTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+/* TODO
+* 1. a dialog box to be displayed if outside of Laoag City
+* 2. runtime permission or upon install permission for location, contact info, send SMS
+* 3. disclaimer and data privacy agreement activity
+* 4. add photo or video of incident / emergency
+* 5. check is SMS is sent within 5 mins., otherwise disable MainActivity() button
+*
+* 6.instead of #2 an Disclaimer and Privacy Composable that will request needed permissions
+* for 1 time only
+*/
+
+fun assembleSMS() {
+    /*Assemble an SMS in CSV format containing contact information, location, emergency code
+    * emergency codes to be discussed
+    * Implement graceful handling of non-granting of name, location and SMS permissions
+    * pseudocode
+    * if (name permission granted)
+    *     name = google account
+    * else
+    *     name = "Anonymous sender"
+    * if (location permission granted)
+    *     location = current location
+    * else
+    *     latitude = "latitude not granted"
+    *     longitude = "longitude not granted"
+    * if (SMS permission granted)
+    *     SendSMS()
+    * else
+    *     ask for permission until granted
+    */
+    TODO()
+}
+
+@Composable
+fun PrivacyAndDisclaimer() {
+    TODO()
+}
+
+@Composable
+fun RadioButtonWithIcon() {
+    val radioOptionsStringIconRes = listOf(
+        Pair(R.drawable.ic_baseline_local_police_24, stringResource(id = R.string.police)),
+        Pair(R.drawable.ic_baseline_local_police_24, stringResource(id = R.string.medical)),
+        Pair(R.drawable.ic_baseline_local_police_24, stringResource(id = R.string.rescue)),
+        Pair(R.drawable.ic_baseline_local_police_24, stringResource(id = R.string.fire)),
+        Pair(R.drawable.ic_baseline_local_police_24, stringResource(id = R.string.other))
+    )
+    val selectedValue = remember { mutableStateOf("") }
+//    Text(text = "Selected value: ${selectedValue.value.ifEmpty { "NONE" }}")
+    radioOptionsStringIconRes.forEach { item ->
+        val selectedColor = if (selectedValue.value == item.second) {
+            MaterialTheme.colors.secondary
+        } else {
+            MaterialTheme.colors.primary
+        }
+        val selectedBackgroundColor = if (selectedValue.value == item.second) {
+            MaterialTheme.colors.secondary.copy(alpha = .5f)
+        } else {
+            Color.LightGray
+        }
+        Surface(
+            shape = MaterialTheme.shapes.small,
+            color = selectedBackgroundColor,
+            border = BorderStroke(
+                width = 2.dp,
+                color = selectedColor
+            ),
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .selectable(
+                        selected = (selectedValue.value == item.second),
+                        onClick = {
+                            selectedValue.value = item.second
+                        },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp)
+            ) {
+                IconToggleButton(
+                    checked = selectedValue.value == item.second,
+                    onCheckedChange = { selectedValue.value = item.second },
+                    modifier = Modifier.size(56.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(item.first),
+                        contentDescription = null,
+                        tint = selectedColor,
+                        modifier = Modifier
+                            .width(56.dp)
+                            .height(56.dp)
+                            .clip(MaterialTheme.shapes.medium)
+
+                    )
+                }
+                Text(
+                    text = item.second,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainAppActivity() {
+    val mainButtonClick = remember { mutableStateOf(false) }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.app_header,
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(128.dp)
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp)
+//                    .clip(MaterialTheme.shapes.small)
+            )
+/*
+            Text(
+                text = stringResource(id = R.string.app_title),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, bottom = 16.dp)
+            )
+*/
+            RadioButtonWithIcon()
+
+            Button(
+// see TODO # 5
+                onClick = { mainButtonClick.value = !mainButtonClick.value },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp, bottom = 4.dp, start = 4.dp, end = 4.dp)
+            )
+            {
+                Text(
+                    text = "String Literal here",
+                    style = MaterialTheme.typography.h6
+                )
+//                Icon(Icons.Filled.List, contentDescription = null)
+            }
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = false)
+@Composable
+fun DefaultPreview() {
+    LaoagAlertTheme {
+        MainAppActivity()
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
---
+++ app/src/main/java/ph/gov/laoagcity/laoagalert/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package ph.gov.laoagcity.laoagalert.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val BlueBaro100 = Color(0xFF1185E0)
+val RedBaro100 = Color(0xFFC42626)
+val BlueBaro200 = Color(0xFF1150DD)
+val RedBaro200 = Color(0xFFC41818)
